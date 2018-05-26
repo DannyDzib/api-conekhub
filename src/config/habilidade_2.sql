b27e3f67d0870ef3622876b15bdfbd1c1f820d36
Henrry Ramirez <contactoraygan@gmail.com>
2018-05-25 22:21:16 -0500

    quite vendor

--- src/config/habilidade_2.sql
+++ src/config/habilidade_2.sql
@@ -9,14 +9,23 @@ CREATE TABLE `habilidades` (
 
 use conekhub_test;
 
-CREATE TABLE `usuario_has_habilidades` (
+CREATE TABLE `usuario_has_seguidores` (
   `id_usuario` int,
   `id_habilidad` int,
   FOREIGN KEY (`id_usuario`) REFERENCES usuarios(`id_usuario`),
-  FOREIGN KEY (`id_habilidad`) REFERENCES habilidades(`id_habilidad`)
+  FOREIGN KEY (`id_habilidad`) REFERENCES seguidores(`id_habilidad`)
 );
 
 use conekhub_test;
+
+
+CREATE TABLE friendlist (               
+    id_usuario int PRIMARY KEY,             
+    id_usuario_FK int PRIMARY KEY,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+    FOREIGN KEY (id_usuario_FK) REFERENCES usuarios(id_usuario)
+);  
+
 
 SELECT habilidad from usuario_has_habilidades Uh
 INNER JOIN usuarios U
